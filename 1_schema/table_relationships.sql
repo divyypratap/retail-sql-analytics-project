a5dcbff9491b0086ec462d64de6bcf05976976ee
divyypratap <divyy.asdev1@gmail.com>
2026-01-15 22:53:57 +0530

    Add files via upload

--- 1_schema/table_relationships.sql
+++ 1_schema/table_relationships.sql
@@ -1,1 +1,27 @@
+-- =====================================
+-- TABLE RELATIONSHIPS (FOREIGN KEYS)
+-- =====================================
 
+-- Customers → Orders
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id);
+
+-- Orders → Order_Items
+ALTER TABLE order_items
+ADD CONSTRAINT fk_order_items_orders
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
+
+-- Products → Order_Items
+ALTER TABLE order_items
+ADD CONSTRAINT fk_order_items_products
+FOREIGN KEY (product_id)
+REFERENCES products(product_id);
+
+-- Orders → Payments
+ALTER TABLE payments
+ADD CONSTRAINT fk_payments_orders
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
